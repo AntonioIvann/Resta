00f2e466883c3a96bbb7d6f37c415d7e90f2b14b
unknown <rantonioivann@gmail.com>
2024-05-02 18:29:05 -0600

    RestaPriv2

--- MainResta.java
+++ MainResta.java
@@ -7,10 +7,12 @@ public class MainResta {
         int n1 = re.nextInt();
         System.out.println("Dame el segundo valor: ");
         int n2 = re.nextInt();
-        Resta re1 = new Resta();
-        re1.n1 = n1;
-        re1.n2 = n2;
-        System.out.println("El resultado es: " + re1.mostrarResultado());
 
+        Resta re1 = new Resta();
+        re1.setN1(n1);
+        re1.setN2(n2);
+        re1.mostrarResultado();
+        System.out.println("El resultado de la resta es: " + re1.mostrarResultado());
+        System.out.println(re1.toString());
     }
 }
