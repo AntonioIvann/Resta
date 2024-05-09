55119d8af2040f3e87f9bdc180e93ce23a7a7fc5
unknown <rantonioivann@gmail.com>
2024-05-08 21:25:17 -0600

    v3

--- MainResta.java
+++ MainResta.java
@@ -1,18 +1,28 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importa la clase Scanner del paquete java.util
 
-public class MainResta {
-    public static void main(String[] args) {
-        Scanner re = new Scanner(System.in);
-        System.out.println("Dame el primer valor: ");
-        int n1 = re.nextInt();
-        System.out.println("Dame el segundo valor: ");
-        int n2 = re.nextInt();
+public class MainResta { // Declara una clase pública llamada MainResta
+    public static void main(String[] args) { // Método principal, punto de entrada del programa
+        Scanner re = new Scanner(System.in); // Crea un objeto Scanner llamado 're' para leer la entrada del usuario
 
-        Resta re1 = new Resta();
+        System.out.println("Dame el primer valor: "); // Solicita al usuario que ingrese el primer valor
+        int n1 = re.nextInt(); // Lee el primer valor proporcionado por el usuario
+
+        System.out.println("Dame el segundo valor: "); // Solicita al usuario que ingrese el segundo valor
+        int n2 = re.nextInt(); // Lee el segundo valor proporcionado por el usuario
+
+        Resta re1 = new Resta(); // Crea un objeto de la clase Resta llamado 're1'
+
+        // Establece los valores n1 y n2 en el objeto 're1' utilizando los métodos setN1() y setN2()
         re1.setN1(n1);
         re1.setN2(n2);
+
+        // Llama al método mostrarResultado() del objeto 're1' para mostrar el resultado de la resta
         re1.mostrarResultado();
+
+        // Imprime el resultado de la resta utilizando el método mostrarResultado() del objeto 're1'
         System.out.println("El resultado de la resta es: " + re1.mostrarResultado());
+
+        // Imprime la representación de cadena del objeto 're1' utilizando el método toString()
         System.out.println(re1.toString());
     }
 }
--- Resta.java
+++ Resta.java
@@ -1,37 +1,42 @@
-public class Resta {
-    private int n1, n2, res;
+public class Resta { // Declaración de la clase Resta
+    private int n1, n2, res; // Declaración de tres variables privadas de tipo entero: n1, n2 y res (resultado)
 
+    // Método para mostrar el resultado de la resta
     public int mostrarResultado() {
-        res = n1 - n2;
-        return res;
+        res = n1 - n2; // Realiza la operación de resta entre n1 y n2 y guarda el resultado en la variable res
+        return res; // Devuelve el resultado de la resta
     }
 
+    // Métodos getter y setter para la variable n1
     public int getN1() {
-        return n1;
+        return n1; // Devuelve el valor de n1
     }
 
     public void setN1(int n1) {
-        this.n1 = n1;
+        this.n1 = n1; // Establece el valor de n1
     }
 
+    // Métodos getter y setter para la variable n2
     public int getN2() {
-        return n2;
+        return n2; // Devuelve el valor de n2
     }
 
     public void setN2(int n2) {
-        this.n2 = n2;
+        this.n2 = n2; // Establece el valor de n2
     }
 
+    // Métodos getter y setter para la variable res
     public int getRes() {
-        return res;
+        return res; // Devuelve el valor de res
     }
 
     public void setRes(int res) {
-        this.res = res;
+        this.res = res; // Establece el valor de res
     }
 
+    // Método toString para representar el objeto como una cadena de texto
     @Override
     public String toString() {
-        return "Suma [n1=" + n1 + ", n2=" + n2 + ", res=" + res + "]";
+        return "Resta [n1=" + n1 + ", n2=" + n2 + ", res=" + res + "]"; // Devuelve una cadena que muestra los valores de n1, n2 y res
     }
 }
